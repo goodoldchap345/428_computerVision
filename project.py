ed286c54c6bf77553a6f55aac2990dfe810451ee
Aidan Patrick Barnsdale <aba177@cs19103bs.canterbury.ac.nz>
2020-05-17 17:48:29 +1200

    modified to handle realsense images

--- project.py
+++ project.py
@@ -1,14 +1,41 @@
 # harris.py
 
+import sys
 import cv2
 import numpy as np
-#import pyrealsense2 as rs
+import pyrealsense2 as rs
 
-cap = cv2.VideoCapture('doorway.avi')  # Open the first camera connected to the computer.
+# ------- For standard video file --------
+#cap = cv2.VideoCapture('doorway.avi')  # Open the first camera connected to the computer.
+#if cap.read()[1] is None:
+    #print("file not found")
+    #sys.exit()
+# ----------------------------------------
+
+
+path_to_bag = "3.bag"  # Location of input file.
+
+config = rs.config()
+# This specifies that we are loading pre-recorded data 
+# rather than using a live camera.
+config.enable_device_from_file(path_to_bag)
+pipeline = rs.pipeline()  # Create a pipeline
+profile = pipeline.start(config)  # Start streaming
+align = rs.align(rs.stream.color)  # Create the alignment object
+    
 
 while True:
-    ret, frame_raw = cap.read()
-    frame = frame_raw[800:1200, 200:700]
+    #ret, frame_raw = cap.read()
+    #frame = frame_raw[800:1200, 200:700]
+    
+    # Get frameset of color and depth and align them.
+    frames = pipeline.wait_for_frames()
+    aligned_frames = align.process(frames)
+
+    # Get the frames
+    depth_image = np.asanyarray(aligned_frames.get_depth_frame().get_data())
+    color_image = np.asanyarray(aligned_frames.get_color_frame().get_data())
+    frame = color_image
 
     #throw a canny at it
     threshold1 = 75
@@ -24,6 +51,7 @@ while True:
                             minLineLength=minLineLength, maxLineGap=maxLineGap)
 
     # For each line that was detected, draw it on the img.
+    print(lines)
     if lines is not None:
         for line in lines:
             for x1,y1,x2,y2 in line:
@@ -45,5 +73,5 @@ while True:
     if cv2.waitKey(100) & 0xFF == ord('q'):  # Close the script when q is pressed.
         break
 
-cap.release()
+#cap.release()
 cv2.destroyAllWindows()
